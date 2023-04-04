9e031ce53c3f910435fca36cee34c137a73b67bd
Chris <cralpizar@gmail.com>
2023-04-03 22:04:09 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
+
+btn.addEventListener("click", sumarInputValues);
+
+function sumarInputValues(event) {
+    pResult.innerText = "Resultado: " + input1.value + input2.value;
+}
